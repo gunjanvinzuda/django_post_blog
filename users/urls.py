bc045db1c0970e0261af155adf22624e3571f794
Gunjan Vinzuda <58952163+gunjanvinzuda@users.noreply.github.com>
2024-11-17 22:52:39 +0530

    Fixed logout view

--- users/urls.py
+++ users/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from .views import PatchLogoutView
 from . import views
 
 urlpatterns = [
 	path('register/', views.register, name="register"),
 	path('profile/', views.profile, name="profile"),
 	path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="login" ),
-	path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'),name="logout"),
+	path('logout/', PatchLogoutView.as_view(template_name="users/logout.html"), name='logout'),
 	path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),name="password_reset"),
 	path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),name="password_reset_complete"),
 	path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),name="password_reset_done"),
